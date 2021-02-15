d54ed4336d4fb45c3f0e6a8f1461fb1ff1774005
Colten Appleby <colten96@gmail.com>
2021-02-15 23:39:26 +0000

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,4 +8,14 @@
 
 def select_winner(passengers)
   # add the code snippet here!
+  
+  winner = ''
+  passengers.each do |suite, name|
+    if suite == :suite_a and name.start_with?("A") or name.start_with?("a")
+      winner = name
+    end
+  end
+  
+  return winner
+  
 end
